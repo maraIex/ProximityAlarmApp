c82e8a2b6c96faa8e11f3a9f13b3d68164c888ab
maraIex <maraleks2005@gmail.com>
2025-03-11 20:41:20 +0400

    First commit for Drawer: - Added Drawer button and Drawer itself - Added Inter font from interface design - Set up NavigationView behaviour

--- app/src/main/java/com/example/proximityalarmapp/MainActivity.kt
+++ app/src/main/java/com/example/proximityalarmapp/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.proximityalarmapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Поиск элементов в интерфейсе по id
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.navigation_view)
+        val btnMenu: ImageButton = findViewById(R.id.btn_menu)
+
+        // Если кнопка шторки нажата
+        btnMenu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START) // Открываем меню
+        }
+
+        // Обработка нажатий на меню
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_alarms -> {
+                    //startActivity(Intent(this, AlarmsActivity::class.java)) Смена Activity на AlarmsActivity
+                    drawerLayout.closeDrawers() // Закрываем меню после выбора
+                    true
+                }
+                R.id.nav_settings -> {
+                    //startActivity(Intent(this, SettingsActivity::class.java)) // Смена Activity на SettingsActivity
+                    drawerLayout.closeDrawers()
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
